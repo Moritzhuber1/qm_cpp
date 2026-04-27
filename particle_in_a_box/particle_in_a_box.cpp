772bc6bd8c69c0a7b08d82fa1bc309320b3071b6
Thibaut Göritz <thibautgoritz@MacBookPro.fritz.box>
2026-04-27 15:57:04 +0200

    add set_length, round_to and print function

--- particle_in_a_box/particle_in_a_box.cpp
+++ particle_in_a_box/particle_in_a_box.cpp
@@ -1,37 +1,74 @@
 #include <iostream>
 #include <numbers>
 #include <math.h>
-using namespace std; 
 
 namespace phys_const {
-    const double h = 6.626e-34;
+    const double h = 6.62607015e-34;
     const double h_bar = h/(2*M_PI);  
 }
+
 using namespace phys_const;
+using namespace std; 
+
+// ====================================
+//           - functions -
+// ====================================
+
+
+template<typename... Args>
+void print(Args&&... args) {
+	((cout << args), ...);
+}
+
+template <typename T>
+double round_to(T a, int dec) {
+	double mult = 1.0;
+	for (int i = 0; i < dec; i++) {
+		mult *= 10.0;
+	}
+
+	double scaled = a * mult;
+	double rounded = round(scaled);
+
+	return rounded / mult;
+}
+
+// ====================================
+//           - Particle class -
+// ====================================
 
 class ParticleInABox {
     private:
 
-    float length;
-    float mass = 1,6735575*10e-27;   
-    float h_bar = h/(2*M_PI);
+    	float calc_energy(float length, float mass, int n) {
+        	return (pow((n * M_PI * h_bar),2)) / (2 * mass * pow(length,2));
+    	}
+
+    	float length;
+    	float mass = 1.6735575*10e-27;   
+    	float h_bar = h/(2*M_PI);
+		int n;
+
+		float energy;
     
     public:
 
-    float energie(float length) {
-        int n = 1;
-        float mass = 9.109e-31; 
-        return (pow(n,2)*pow(M_PI,2)*pow(h_bar,2))/(2*mass*pow(length,2));
-    }
+		void set_length(float new_length) {
+			length = new_length;
+			print("New length is set to: ", round_to(length, 2), " nm\n");
+		}
 };
+
+// ====================================
+//           - main function -
+// ====================================
 
 int main() {
     float length;
-    cout << "gib länge der Box:" << "\n";
+    cout << "Length of the box in [nm]: " << "\n";
     cin >> length;
     ParticleInABox partile_1; 
-    float energies = partile_1.energie(length);
-    cout << energies << "super sache\n"; 
+    partile_1.set_length(length);
     return 0;
 } 
 
